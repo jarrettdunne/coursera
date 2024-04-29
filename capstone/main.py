855923e21be288f7a21362cd21e0afd2c9915ead
jarrettdunne <jarrettpdunne@gmail.com>
2024-04-29 14:40:57 -0500

    random test code

--- capstone/main.py
+++ capstone/main.py
@@ -1,6 +1,12 @@
 import requests 
 import pandas as pd
 
+for i in pd.DataFrame():
+    print(i)
+
+import folium
+
+fig = folium.Figure()
 
 if __name__ == '__main__':
     url = 'https://api.spacexdata.com/v4/launches/past'
